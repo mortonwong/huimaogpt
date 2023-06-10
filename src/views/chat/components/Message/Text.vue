aea8271ac6824c8d39ace93589d44ba33e999aab
morton.huang <morton.huang@corp.to8to.com>
2023-06-11 03:10:16 +0800

    katex

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -1,10 +1,10 @@
 <script lang="ts" setup>
 import { computed, defineEmits, onMounted, onUnmounted, onUpdated, ref } from 'vue'
 import MarkdownIt from 'markdown-it'
-import mdKatex from '@traptitech/markdown-it-katex'
 import mila from 'markdown-it-link-attributes'
 import hljs from 'highlight.js'
-
+import katex from 'katex'
+import mdKatex from '@/utils/markdown-it-katex'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 import { t } from '@/locales'
 import { copyToClip } from '@/utils/copy'
@@ -40,7 +40,17 @@ const mdi = new MarkdownIt({
 })
 
 mdi.use(mila, { attrs: { target: '_blank', rel: 'noopener' } })
-mdi.use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
+mdi.use(mdKatex, {
+  delimiters: [
+    { left: '$$', right: '$$', display: true },
+    { left: '$', right: '$', display: false },
+    // 添加自定义的行内公式标记符
+    { left: '%%', right: '%%', display: false },
+  ],
+  blockClass: 'katexmath-block rounded-md p-[10px]',
+  errorColor: ' #cc0000',
+  katex,
+})
 
 const wrapClass = computed(() => {
   return [
--- vite.config.ts
+++ vite.config.ts
@@ -50,12 +50,12 @@ export default defineConfig((env) => {
           entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
           assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
           manualChunks(id) {
-            if (id.includes("node_modules")) {
+            if (id.includes('node_modules')) {
               // 让每个插件都打包成独立的文件
-              return id .toString() .split("node_modules/")[1] .split("/")[0] .toString(); 
+              return id.toString().split('node_modules/')[1].split('/')[0].toString()
             }
-          }
-        }
+          },
+        },
       },
       reportCompressedSize: false,
       sourcemap: false,
